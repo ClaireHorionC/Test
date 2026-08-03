890ee5b33b4177b5682203300f25d371848592d7
Mat <sabben@insa-toulouse.fr>
2026-08-03 15:46:16 +0200

    corrected ANOTHER import

--- UI/TerminalManager.js
+++ UI/TerminalManager.js
@@ -1,6 +1,6 @@
 import gameEngineInstance from '../GameLogic/GameEngine.js';
 import uiManagerInstance from './UIManager.js';
-import { ENIGMA_IDS } from '../Utils/Constant.js';
+import { ENIGMA_IDS } from '../../Utils/Constant.js';
 import { HELP_IDS } from '../../Utils/Constant.js';
 import { ENIGMA_STATUS } from '../../Utils/Constant.js';
 
