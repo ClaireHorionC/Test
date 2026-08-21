781cfd3c47d4bbb8003ec3421d6246a887f0d055
ClaireHorionC <horion@insa-toulouse.fr>
2026-08-21 14:36:36 +0200

    Update ArucoEnigma.js

--- GameLogic/Enigmas/ArucoEnigma.js
+++ GameLogic/Enigmas/ArucoEnigma.js
@@ -9,7 +9,7 @@ export class ArucoEnigma extends Enigma {
 
         // Configuration métier du jeu
         this.positionTolerance = 10;
-        this.nbIterations = 100;
+        this.nbIterations = 200;
         this.maxID = 100;
 
         this.dictCards = {
